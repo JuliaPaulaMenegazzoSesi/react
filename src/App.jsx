eba25540027fe2616ff9117f188b7e20c14c2e4a
JuliaPaulaMenegazzoSesi <jpmgithubsesi@gmail.com>
2024-03-26 09:45:41 -0300

    dfdsf

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
+import img01 from "./assets/imgs/foto01.png"
+import img02 from "./assets/imgs/foto02.png"
+import img03 from "./assets/imgs/foto03.png"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Insanidade</h1>
+      <Card
+      title="Paisagem 1"
+      image={img01}
+      text01="a"
+      text02="Inflamação"
+      />
+      <Card
+      title="Paisagem 2"
+      image={img02}
+      text01="b"
+      text02="Continuar"
+      />
+      <Card
+      title="Paisagem 3"
+      image={img03}
+      text01="c"
+      text02="Entornar"
+      />
     </>
   )
 }

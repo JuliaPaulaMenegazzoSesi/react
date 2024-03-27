573f4f7ad896dd3a42b8e7494a959a4b417e5ef0
JuliaPaulaMenegazzoSesi <jpmgithubsesi@gmail.com>
2024-03-27 09:59:13 -0300

    Mapinha sem iframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Tilt } from 'react-tilt'
 
 
-import Card from './components/card'
+import Card from './components/card.jsx'
+import Menu from './components/Menu.jsx'
 import img01 from "./assets/imgs/foto01.png"
 import img02 from "./assets/imgs/foto02.png"
 import img03 from "./assets/imgs/foto03.png"
@@ -12,48 +13,50 @@ import style from './App.module.css'
 function App() {
 
   const defaultOptions = {
-    reverse:        true,  // reverse the tilt direction
-    max:            100,     // max tilt rotation (degrees)
-    perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
-    scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
-    speed:          1000,   // Speed of the enter/exit transition
-    transition:     true,   // Set a transition on enter/exit.
-    axis:           null,   // What axis should be disabled. Can be X or Y.
-    reset:          true,    // If the tilt effect has to be reset on exit.
-    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+    reverse: true,  // reverse the tilt direction
+    max: 20,     // max tilt rotation (degrees)
+    perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale: 1.1,    // 2 = 200%, 1.5 = 150%, etc..
+    speed: 1000,   // Speed of the enter/exit transition
+    transition: true,   // Set a transition on enter/exit.
+    axis: null,   // What axis should be disabled. Can be X or Y.
+    reset: true,    // If the tilt effect has to be reset on exit.
+    easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
   }
-  
+
 
   return (
-    <div className={style.section01}>
-      <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
-      <div><Card
-      title="Paisagem 1"
-      image={img01}
-      text01="a"
-      text02="Inflamação"
-      /></div>
-    </Tilt>
-    <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
-      <div><Card
-      title="Paisagem 2"
-      image={img02}
-      text01="b"
-      text02="Continuar"
-      /></div>
-    </Tilt>
-    <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
-      <div><Card
-      title="Paisagem 3"
-      image={img03}
-      text01="c"
-      text02="Entornar"
-      /></div>
-    </Tilt>
-      
-      
-      
-    </div>
+    <>
+      <Menu />
+
+      <div className={style.section01}>
+        <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+          <div><Card
+            title="Paisagem 1"
+            image={img01}
+            text01="a"
+            text02="Inflamação"
+          /></div>
+        </Tilt>
+        <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+          <div><Card
+            title="Paisagem 2"
+            image={img02}
+            text01="b"
+            text02="Continuar"
+          /></div>
+        </Tilt>
+        <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+          <div><Card
+            title="Paisagem 3"
+            image={img03}
+            text01="c"
+            text02="Entornar"
+          /></div>
+        </Tilt>
+      </div>
+
+    </>
   )
 }
 
---
+++ src/Contact.jsx
@@ -0,0 +1,34 @@
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import 'leaflet-defaulticon-compatibility';
+import Menu from './components/Menu'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+export const Contact = () => {
+    const geoData = ({lat: -25.4249668, lng: -49.2748809})
+    return (
+        
+        <>
+            <Menu />
+            <div>
+                <h1>Contato</h1>
+                <div>
+                    <h2>Mapa</h2>
+                    <div>
+                        <MapContainer center={[geoData.lat, geoData.lng]} zoom={13} scrollWheelZoom={false} style={{width: "100%", height: "700px"}}>
+                            <TileLayer
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={[geoData.lat, geoData.lng]}>
+                                <Popup>
+                                R. Paula Gomes, 282 - São Francisco<br />Curitiba - PR, 80510-070
+                                </Popup>
+                            </Marker>
+                        </MapContainer>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}

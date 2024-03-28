dc4bd423dd010d30b58e55a106165d00e713bc11
JuliaPaulaMenegazzoSesi <jpmgithubsesi@gmail.com>
2024-03-28 11:30:49 -0300

    Zap

--- src/Calc.jsx
+++ src/Calc.jsx
@@ -1,13 +1,74 @@
 import Menu from "./components/Menu"
 import style from './Calc.module.css'
+import { useEffect, useState } from "react"
 
 export const Calc = () => {
-    return(
+    const [numero1, setNumero1] = useState()
+    const [numero2, setNumero2] = useState()
+    const [respSoma, setRespSoma] = useState()
+    const [respSub, setRespSub] = useState()
+    const [respMult, setRespMult] = useState()
+    const [respDiv, setRespDiv] = useState()
+
+    // useEffect(() => {
+    //     if (numero1 !== undefined && numero2 !== undefined) {
+    //         setRespSoma(parseFloat(numero1) + parseFloat(numero2))
+    //         setRespSub(parseFloat(numero1) - parseFloat(numero2))
+    //         setRespMult(parseFloat(numero1) * parseFloat(numero2))
+    //         setRespDiv(parseFloat(numero1) / parseFloat(numero2))
+    //     }
+    // }, [numero1, numero2])
+
+    const handleSoma = () => setRespSoma(parseFloat(numero1) + parseFloat(numero2))
+    const handleSub = () => setRespSub(parseFloat(numero1) - parseFloat(numero2))
+    const handleMult = () => setRespMult(parseFloat(numero1) * parseFloat(numero2))
+    const handleDiv = () => setRespDiv(parseFloat(numero1) / parseFloat(numero2))
+
+    const handleAll = (a, b) => {
+        setRespSoma(parseFloat(a) + parseFloat(b))
+        setRespSub(parseFloat(a) - parseFloat(b))
+        setRespMult(parseFloat(a) * parseFloat(b))
+        setRespDiv(parseFloat(a) / parseFloat(b))
+    }
+
+    const msgErro = "Digite um número válido"
+    return (
         <>
-        <Menu />
-        <div class={style.sectionCalc}>
-            <h1>Cálculos</h1>
-        </div>
+            <Menu />
+            <div className={style.sectionCalc}>
+                <h1>Cálculos</h1>
+                <div>
+                    <label htmlFor="n1">Número 1:</label>
+                    <input type="text" id="n1" value={numero1} onChange={(e) => setNumero1(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="n2">Número 2:</label>
+                    <input type="text" id="n2" value={numero2} onChange={(e) => setNumero2(e.target.value)} />
+                </div>
+                <button onClick={() => handleAll(numero1, numero2)}>Calcular</button>
+                <div>
+                    <div>
+                        <h1>Soma</h1>
+                        {!isNaN(respSoma) ? respSoma.toFixed(2): msgErro}
+                        <button onClick={handleSoma}>Somar</button>
+                    </div>
+                    <div>
+                        <h1>Subtração</h1>
+                        {!isNaN(respSub) ? respSub.toFixed(2): msgErro}
+                        <button onClick={handleSub}>Subtrair</button>
+                    </div>
+                    <div>
+                        <h1>Multiplicação</h1>
+                        {!isNaN(respMult) ? respMult.toFixed(2): msgErro}
+                        <button onClick={handleMult}>Multiplicar</button>
+                    </div>
+                    <div>
+                        <h1>Divisão</h1>
+                        {!isNaN(respDiv) ? respDiv.toFixed(2): msgErro}
+                        <button onClick={handleDiv}>Dividir</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,34 +1,80 @@
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 import 'leaflet-defaulticon-compatibility';
+
 import Menu from './components/Menu'
+
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
 import style from './Contact.module.css'
 
+import { useState } from "react"
+
 export const Contact = () => {
-    const geoData = ({lat: -25.4249668, lng: -49.2748809})
+    const geoData = ({ lat: -25.4249668, lng: -49.2748809 })
+
+    const defaultPhoneNumber = "5541999999999"
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: ""
+    })
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value})
+    }
+
+    const handleZap = () => {
+        const {name, email, message} = formData;
+
+        const urlZapZap = `https://api.whatsapp.com/send?phone=${defaultPhoneNumber}&text=
+        Nome:%20${name}%0D%0A
+        E-mail:%20${email}%0D%0A
+        Mensagem:%20${message}%0D%0A`
+
+        window.open(urlZapZap, '_blank')
+    }
     return (
-        
+
         <>
             <Menu />
             <div className={style.sectionContact}>
-                <h1>Contato</h1>
+                <div>
+                    <h2>Contato</h2>
+                    <div>
+                        <label htmlFor="name">Nome</label>
+                        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required/>
+                    </div>
+                    <div>
+                        <label htmlFor="email">E-mail</label>
+                        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required/>
+                    </div>
+                    <div>
+                        <label htmlFor="message">Mensagem</label>
+                        <textarea name="message" id="message" value={formData.message} cols="30" rows="10" onChange={handleChange} required></textarea>
+                    </div>
+                    <button onClick={handleZap}>Enviar mensagem</button>
+                </div>
                 <div>
                     <h2>Mapa</h2>
                     <div>
-                        <MapContainer center={[geoData.lat, geoData.lng]} zoom={13} scrollWheelZoom={false} style={{width: "100%", height: "700px"}}>
+                        <MapContainer center={[geoData.lat, geoData.lng]} zoom={13} scrollWheelZoom={false} style={{ width: "100%", height: "700px" }}>
                             <TileLayer
                                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                             />
                             <Marker position={[geoData.lat, geoData.lng]}>
                                 <Popup>
-                                R. Paula Gomes, 282 - São Francisco<br />Curitiba - PR, 80510-070
+                                    R. Paula Gomes, 282 - São Francisco<br />Curitiba - PR, 80510-070
                                 </Popup>
                             </Marker>
                         </MapContainer>
                     </div>
+
                 </div>
+
             </div>
         </>
     )
